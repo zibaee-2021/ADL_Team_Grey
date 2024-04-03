22c12b2f5e46a7080bedd216d5a1be5ce1b1229e
zibaee-2021 <shahinjzibaee@hotmail.co.uk>
2024-04-03 13:08:43 +0100

    add unittest for iou metric

--- tests/MVAETests.py
+++ tests/MVAETests.py
@@ -1,6 +1,8 @@
-import unittest
 from unittest import TestCase
-import src.MVAE_2903_working as mvae
+import src.older_versions.MVAE_2903_working as mvae
+import torch
+import torch.nn.functional as F
+import src.IoUMetric as ioumet
 
 
 class MVAETests(TestCase):
@@ -8,7 +10,7 @@ class MVAETests(TestCase):
     parameters = {
         # image
         "image_size": 224,  # number of pixels square
-        "num_channels": 3,  #  RGB image -> 3 channels
+        "num_channels": 3,  # RGB image -> 3 channels
         "patch_size": 16,  # must be divisor of image_size
         # vision transformer encoder
         "vit_num_features": 768,  # number of features created by the vision transformer
@@ -24,7 +26,33 @@ class MVAETests(TestCase):
         "segmenter_hidden_dim_2": 1024,
     }
 
-    def test_mask_tester(self, patch_masker, image_file):
+    # def test_mask_tester(self):
+    #
+    #
+    #     mvae.mask_tester(patch_masker, image_file)
 
+    def test_custom_iou_loss(self):
+        torch.manual_seed(42)
+        preds = torch.rand((2, 3, 2, 2), requires_grad=True) # (batch_size, channel, height, width)
+        grount_truth = torch.randint(0, 3, (2, 1, 2, 2), dtype=torch.long)
+        expected = torch.Tensor(1.4408)
+        actual = ioumet.IoULoss(softmax=True)(preds, grount_truth)
+        actual = torch.Tensor(actual.item())
+        print(actual)
+        self.assertEqual(expected, actual)
 
-        mvae.mask_tester(patch_masker, image_file)
+    def test_custom_iou_metric(self):
+        torch.manual_seed(42)
+        # preds = torch.rand((2, 3, 2, 2), requires_grad=True) # (batch_size, channel, height, width)
+        preds_logits = torch.randn(1, 3, 4, 4)  # 1, 3, 4, 4 are the number of number to generate in each of the 4 dims
+        # preds_probs = F.softmax(preds_logits, dim=1)
+        ground_truth = torch.tensor([[[0, 1, 2, 1], [1, 2, 0, 2], [2, 0, 1, 0], [0, 2, 1, 2]]], dtype=torch.long)
+        print(f'ground_truth.shape={ground_truth.shape}')   # shape (1, 4, 4)
+        ground_truth = ground_truth.unsqueeze(0)  # shape (1, 1, 4, 4)
+        print(f'ground_truth.shape={ground_truth.shape}')
+        # ground_truth = torch.randint(0, 3, (2, 1, 2, 2), dtype=torch.long)
+        # actual = ioumet.iou_metric(preds_probs, ground_truth, preds_are_logits=False)
+        actual = ioumet.iou_metric(preds_logits, ground_truth, preds_are_logits=True)
+        print(f'actual={actual}')
+        expected = 0.1706485003232956
+        self.assertEqual(expected, actual)
